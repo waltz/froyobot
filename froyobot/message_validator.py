4a5b1a947ba35508d8d58d3650e47ca78821c512
CJ Bryan <cj@cjbryan.io>
2021-06-02 13:13:45 -0700

    Don't show messages with fewer than 10 characters

--- froyobot/message_validator.py
+++ froyobot/message_validator.py
@@ -3,4 +3,7 @@ import re
 def isMessageValid(currentMessage):
     """ Determines if a message is valid. """
 
+    if len(currentMessage) < 10:
+        return False
+
     return re.search('.*{', currentMessage) == None
--- tests/test_message_validator.py
+++ tests/test_message_validator.py
@@ -6,7 +6,7 @@ from froyobot.message_validator import isMessageValid
 class TestMessageValidator(unittest.TestCase):
 
     def test_is_valid_message(self):
-        self.assertEqual(isMessageValid("salami"), True)
+        self.assertEqual(isMessageValid("salami hams"), True)
 
     def test_for_brackets(self):
         """
@@ -15,3 +15,8 @@ class TestMessageValidator(unittest.TestCase):
         """
 
         self.assertEqual(isMessageValid("phong-bread{stuff}"), False)
+
+    def test_minimum_length(self):
+        """ Messages should be kinda long-ish. Not just a single word. """
+
+        self.assertEqual(isMessageValid("huh"), False)
