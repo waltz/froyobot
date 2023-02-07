f36a4d71d4b69ec7fd9c7c89949498c9cd5c6c1f
CJ Bryan <cj@cjbryan.io>
2023-02-06 23:19:58 -0800

    Toot to mastodon

--- froyobot/send_tweet.py
+++ froyobot/send_tweet.py
@@ -1,30 +1,53 @@
 import os
 
 from twitter import *
+from mastodon import Mastodon
 from dotenv import load_dotenv
 
 from .message_getter import MessageGetter
 
+load_dotenv()
+
 def getStatus():
     return MessageGetter(os.path.dirname(__file__) + '/../resources/data/nimrod').shuffle()
 
-def sendTweet():
-    load_dotenv()
-
+def postToTwitter(status):
     token = os.getenv('TOKEN')
     token_secret = os.getenv('TOKEN_SECRET')
     consumer_key = os.getenv('CONSUMER_KEY')
     consumer_secret = os.getenv('CONSUMER_SECRET')
 
-    status = getStatus()
-
     client = Twitter(auth=OAuth(token, token_secret, consumer_key, consumer_secret))
     response = client.statuses.update(status=getStatus())
 
-    print('Post success.')
-    print(response['text'])
-    print(response['created_at'])
     print('https://twitter.com/i/status/' + str(response['id']))
+
+def postToMastodon(status):
+    api_base_url = os.getenv('MASTODON_API_BASE_URL')
+    access_token = os.getenv('MASTODON_ACCESS_TOKEN')
+
+    client = Mastodon(
+        api_base_url = api_base_url,
+        access_token = access_token,
+    )
+    bots_response = client.toot(status)
+    print(bots_response['uri'])
+
+def sendTweet():
+    status = getStatus()
+    print(status)
+
+    try:
+        postToTwitter(status)
+    except Exception as error:
+        print("Posting to Twitter failed")
+        print(error)
+
+    try:
+        postToMastodon(status)
+    except Exception as error:
+        print("Posting to Mastodon failed")
+        print(error)
 
 if __name__ == "__main__":
     sendTweet()
